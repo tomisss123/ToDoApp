b5aa47c067f9cf8bd168bf67971e237120c2b1e7
D <d@d.d>
2022-09-02 00:46:07 +0200

    lol

--- src/main/java/com/example/todoapp/ToDoAppMain.java
+++ src/main/java/com/example/todoapp/ToDoAppMain.java
@@ -12,26 +12,31 @@ public class ToDoAppMain {
 
 
     public static void createTableUser(Connection connection) throws SQLException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj imię:");
-        String name = scanner.nextLine();
-        System.out.println("Podaj nazwisko:");
-        String surname = scanner.nextLine();
-        System.out.println("Podaj e-mail:");
-        String email = scanner.nextLine();
-        System.out.println("Podaj login:");
-        String login = scanner.nextLine();
-        System.out.println("Podaj hasło:");
-        String password = scanner.nextLine();
-
-        password = BCrypt.hashpw(password, BCrypt.gensalt());
-
         Statement statement = connection.createStatement();
-        String mySql = "CREATE TABLE IF NOT EXISTS user_data ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(128) NOT NULL, surname VARCHAR(255) NOT NULL, login VARCHAR(128) NOT NULL UNIQUE, password CHAR(60) NOT NULL, email VARCHAR(255) UNIQUE)";
-
+        String mySql = "CREATE TABLE IF NOT EXISTS user_data ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(128) NOT NULL, surname VARCHAR(255) NOT NULL, login VARCHAR(128) NOT NULL UNIQUE, password CHAR(60) NOT NULL, email VARCHAR(255) UNIQUE)" ;
         statement.executeUpdate(mySql);
-        String mySql2 = "INSERT INTO user_data (name, surname, login, password, email) VALUES('" + name + "','" + surname + "','" + login + "','" + password + "','" + email + "')";
-        statement.executeUpdate(mySql2);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Logowanie - 1\nRejestracja - 2");
+        if (scanner.nextInt() == 2) {
+            System.out.println("Podaj imię:");
+            Scanner scanner1 = new Scanner(System.in);
+            String name = scanner1.nextLine();
+            System.out.println("Podaj nazwisko:");
+            String surname = scanner1.nextLine();
+            System.out.println("Podaj e-mail:");
+            String email = scanner1.nextLine();
+            System.out.println("Podaj login:");
+            String login = scanner1.nextLine();
+            System.out.println("Podaj hasło:");
+            String password = scanner1.nextLine();
+
+            password = BCrypt.hashpw(password, BCrypt.gensalt());
+            String mySql2 = "INSERT INTO user_data (name, surname, login, password, email) VALUES('" + name + "','" + surname + "','" + login + "','" + password + "','" + email + "')" ;
+            statement.executeUpdate(mySql2);
+        }
+
+
     }
 
     public static void main(String[] args) {
