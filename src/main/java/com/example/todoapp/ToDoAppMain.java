69827aacb32cdf08451e51b14dacb0e83ebcfe1c
Pięćdziesiątka Trygonometryczna <108329148+PiecdziesiatkaTrygonometryczna@users.noreply.github.com>
2022-09-01 14:52:09 +0200

    testowy stol

---
+++ src/main/java/com/example/todoapp/ToDoAppMain.java
@@ -0,0 +1,40 @@
+package com.example.todoapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ToDoAppMain {
+
+
+    public static void createDatabase(Connection connection, String name) throws SQLException {
+        Statement statement = connection.createStatement();
+        String mySql = """
+                CREATE TABLE IF NOT EXISTS name(
+                 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                 name VARCHAR(128) NOT NULL,
+                surname VARCHAR(255) NOT NULL,
+                 login VARCHAR(128) NOT NULL UNIQUE,
+                password CHAR(60) NOT NULL,
+                 email VARCHAR(255) UNIQUE
+                 )
+                 """;
+        statement.executeUpdate(mySql);
+    }
+
+    public static void main(String[] args) {
+        try (Connection connection =
+                     DriverManager.getConnection(
+                             "jdbc:mysql://db4free.net:3306/javatestproje",
+                             "ayumuumuya",
+                             "Javatestproject"
+
+                     )) {
+            createDatabase(connection, "user");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
