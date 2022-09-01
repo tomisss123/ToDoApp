9e681a0d78a50c8a73431d4916f3260d5fc91c4a
D <d@d.d>
2022-09-01 19:16:04 +0200

    commicik testowy

--- src/main/java/com/example/todoapp/HelloApplication.java
+++ src/main/java/com/example/todoapp/HelloApplication.java
@@ -18,6 +18,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
         stage.centerOnScreen();
+        System.out.println("lol");
     }
 
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
