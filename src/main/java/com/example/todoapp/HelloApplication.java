7104b219bae8a0205c94ab9aec14e50b06fee3f3
tomaszboros <tomekboros7@gmail.com>
2022-09-01 12:34:27 +0200

    tescik

--- src/main/java/com/example/todoapp/HelloApplication.java
+++ src/main/java/com/example/todoapp/HelloApplication.java
@@ -15,6 +15,7 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        stage.centerOnScreen();
     }
 
     public static void main(String[] args) {
