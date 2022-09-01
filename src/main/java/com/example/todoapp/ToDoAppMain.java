6c19dee71d479d8f46e3ac719181744553403c9d
tomaszboros <tomekboros7@gmail.com>
2022-09-01 15:14:00 +0200

    poprawki

--- src/main/java/com/example/todoapp/ToDoAppMain.java
+++ src/main/java/com/example/todoapp/ToDoAppMain.java
@@ -11,7 +11,7 @@ public class ToDoAppMain {
     public static void createTableUser(Connection connection) throws SQLException {
         Statement statement = connection.createStatement();
         String mySql = """
-                CREATE TABLE IF NOT EXISTS name(
+                CREATE TABLE IF NOT EXISTS user_data(
                  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                  name VARCHAR(128) NOT NULL,
                 surname VARCHAR(255) NOT NULL,
